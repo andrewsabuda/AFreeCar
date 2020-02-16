b36a89b376bcf2823da14ac43944248bffc1461e
Andrew Sabuda <andrewsabuda@oakland.edu>
2020-02-15 21:55:46 -0500

    Beginning to implement requirements checklist

---
+++ app/src/main/java/com/example/afreecar/RequirementsActivity.java
@@ -0,0 +1,14 @@
+package com.example.afreecar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RequirementsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_requirements);
+    }
+}
---
+++ app/src/main/java/com/example/afreecar/RequirementsAdapter.java
@@ -0,0 +1,4 @@
+package com.example.afreecar;
+
+public class RequirementsAdapter {
+}
