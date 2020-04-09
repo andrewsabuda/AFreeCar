6bd0c32669d4223f04dbde36c1df11b8b61cd7df
Andrew Sabuda <andrewsabuda@oakland.edu>
2020-04-08 20:00:49 -0400

    Space reader is now working with the checklist

--- app/src/main/java/com/example/afreecar/MainActivity.java
+++ app/src/main/java/com/example/afreecar/MainActivity.java
@@ -4,66 +4,32 @@ import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.content.res.Resources;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Color;
-import android.graphics.drawable.Drawable;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Vibrator;
-import android.provider.MediaStore;
-import android.util.Log;
 import android.util.SparseArray;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
-import androidx.core.content.res.ResourcesCompat;
 
 import com.google.android.gms.vision.CameraSource;
 import com.google.android.gms.vision.Detector;
 import com.google.android.gms.vision.barcode.Barcode;
 import com.google.android.gms.vision.barcode.BarcodeDetector;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.firestore.FirebaseFirestore;
 
 import org.opencv.android.BaseLoaderCallback;
-import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
-import org.opencv.android.Utils;
-import org.opencv.core.Core;
-import org.opencv.core.CvType;
-import org.opencv.core.Mat;
-import org.opencv.core.MatOfPoint;
-import org.opencv.core.MatOfPoint2f;
-import org.opencv.core.Point;
-import org.opencv.core.Rect;
-import org.opencv.core.RotatedRect;
-import org.opencv.core.Scalar;
-import org.opencv.core.Size;
-import org.opencv.imgproc.Imgproc;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -72,11 +38,7 @@ public class MainActivity extends AppCompatActivity {
     BarcodeDetector barcodeDetector;
     CameraSource cameraSource;
     Button confirmResult;
-    Button backToGuide;
-    Button openCv;
-    ImageView im; //Your image View
-    double distanceBetween;
-    List<RotatedRect> boxes = new ArrayList<>();
+    Button goToSpaceDetector;
 
     public final String[] kitOneRequirements = { "1,2", "3,4", "5,6" }; // These need to be stored in the database.
     public final String kitOneId = "assembly-requirements-one"; // This also needs to be stored in the database. Still need to implement compatibility for multiple different types of Kits. Right now, only one kit is being used.
@@ -125,106 +87,7 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
         OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION, this, mLoaderCallback);
     }
-    
-    @RequiresApi(api = Build.VERSION_CODES.N)
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);//method retrieves the requestCode , its result and the data containing the pic from system
-        if (requestCode == 1 && resultCode == RESULT_OK) {
-            Bitmap photo = (Bitmap) data.getExtras().get("data"); //get data and casts it into Bitmap photo
 
-            Mat img = new Mat();
-            Utils.bitmapToMat(photo, img);
-
-            Mat imageGray = new Mat();
-
-            //load the image, convert it to grayscale, and blur it slightly
-            Imgproc.cvtColor(img, imageGray, Imgproc.COLOR_RGB2GRAY);
-            Imgproc.GaussianBlur(imageGray, imageGray, new Size(7, 7), 0);
-
-            //perform edge detection, then perform a dilation + erosion to
-            //close gaps in between object edges
-            Mat edged = new Mat();
-            Imgproc.Canny(imageGray, edged, 50, 100);
-            Imgproc.dilate(edged, edged, Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(2,2)));
-            Imgproc.erode(edged, edged, Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(2,2)));
-
-            //find contours in the edge map
-            ArrayList<MatOfPoint> contours = new ArrayList<MatOfPoint>();
-            Mat hierarchy = new Mat();
-            Imgproc.findContours(edged, contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
-
-            //sort the contours from left-to-right and, then initialize the
-            //distance colors and reference object
-            sortContours(contours);
-
-            //loop over the contours individually
-            for (MatOfPoint contour : contours) {
-                //compute the rotated bounding box of the contour
-                RotatedRect box = Imgproc.minAreaRect(new MatOfPoint2f(contour.toArray()));
-                boxes.add(box);
-            }
-
-            Scalar yellow = new Scalar(255, 255, 0);
-            Scalar blue = new Scalar(0, 0, 255);
-
-            Imgproc.circle(img, boxes.get(0).center, 2, yellow);
-
-            if(boxes.size() > 1) {
-                Imgproc.circle(img, boxes.get(1).center, 2, blue);
-                distanceBetween = euclideanDistance(boxes.get(0).center, boxes.get(1).center);
-            }
-
-            Utils.matToBitmap(img, photo);
-
-            im.setImageBitmap(photo);// set photo to imageView
-        }
-        final Toast toast = Toast.makeText(getApplicationContext(), "distance: " + distanceBetween, Toast.LENGTH_SHORT);
-        toast.show();
-    }
-
-    public double euclideanDistance(Point a, Point b){
-        double distance = 0.0;
-        try{
-            if(a != null && b != null){
-                double xDiff = a.x - b.x;
-                double yDiff = a.y - b.y;
-                distance = Math.sqrt(Math.pow(xDiff,2) + Math.pow(yDiff, 2));
-            }
-        }catch(Exception e){
-            System.err.println("Something went wrong in euclideanDistance function in  "+e.getMessage());
-        }
-        return distance;
-    }
-
-    /**
-     * Sorts contours from left to right (assumes a length of 2)
-     *
-     * @param contours
-     *            List of contours to sort
-     */
-    public ArrayList sortContours(ArrayList<MatOfPoint> contours) {
-        if (getLeftMostX(contours.get(0)) > getLeftMostX(contours.get(1))) {
-            contours.add(contours.remove(0));
-        }
-
-        return contours;
-    }
-
-    /**
-     * @param points
-     *            List of points representing a contour
-     * @return The X coordinate of the left-most point in {@code points}
-     */
-    public double getLeftMostX(MatOfPoint points) {
-        List<Point> coords = points.toList();
-        double leftMostX = coords.get(0).x;
-        for (int i = 0; i < coords.size(); i++) {
-            if (coords.get(i).x < leftMostX) {
-                leftMostX = coords.get(i).x;
-            }
-        }
-        return leftMostX;
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -234,22 +97,10 @@ public class MainActivity extends AppCompatActivity {
         cameraPreview = findViewById(R.id.cameraPreview);
         txtResult = findViewById(R.id.txtResult);
         confirmResult = findViewById(R.id.confirmResult);
-        backToGuide = findViewById(R.id.backToGuide);
-        openCv = findViewById(R.id.openCv);
-        im =(ImageView)findViewById(R.id.imageView);
+        goToSpaceDetector = findViewById(R.id.openCv);
 
-
-        final Intent intent = new Intent(MainActivity.this, OpenCV.class);
-
-        openCv.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent img = new Intent (); //Your Intent
-                img.setAction(MediaStore.ACTION_IMAGE_CAPTURE); //the intents action to capture the image
-                startActivityForResult(img,1);//start the activity adding any code .1 in this example
-            }
-        });
-
+        final Intent intent = new Intent(MainActivity.this, RequirementsActivity.class);
+        final Intent spaceDetectorIntent = new Intent(MainActivity.this, OpenCV.class);
         final Intent assembleIntent = getIntent();
         final boolean isAssembling = assembleIntent.getBooleanExtra("isAssembling", false);
 
@@ -306,7 +157,6 @@ public class MainActivity extends AppCompatActivity {
                         txtResult.post(new Runnable() {
                             @Override
                             public void run() {
-                                final Intent intent = new Intent(MainActivity.this, RequirementsActivity.class);
 
                                 //Create vibrate
                                 Vibrator vibrator = (Vibrator) getApplicationContext().getSystemService(Context.VIBRATOR_SERVICE);
@@ -342,8 +192,6 @@ public class MainActivity extends AppCompatActivity {
                                 String[] stepsArray = stepValue.split(",");
                                 String valueScanned = String.valueOf('x');
 
-                                final Intent intent = new Intent(MainActivity.this, RequirementsActivity.class);
-
                                 //Create vibrate
                                 Vibrator vibrator = (Vibrator) getApplicationContext().getSystemService(Context.VIBRATOR_SERVICE);
                                 vibrator.vibrate(1000);
@@ -358,25 +206,15 @@ public class MainActivity extends AppCompatActivity {
 
                                         if((Arrays.toString(scannedValues)).equals("[x, x]")) {
 
-                                            final Toast toast = Toast.makeText(getApplicationContext(), "List item completed", Toast.LENGTH_SHORT);
-                                            toast.show();
-                                            Handler handler = new Handler();
-                                            handler.postDelayed(new Runnable() {
-                                                @Override
-                                                public void run() {
-                                                    toast.cancel();
-                                                }
-                                            }, 800);
+                                            spaceDetectorIntent.putExtra("listPosition", listPosition);
+                                            spaceDetectorIntent.putExtra("kitId", kitOneId);                      //Sending the kit ID value to RequirementsActivity
+                                            spaceDetectorIntent.putExtra("requirementsList", requirementsList);
 
-                                            requirementsList[listPosition] = "completed";
-                                            intent.putExtra("kitId", kitOneId);                      //Sending the kit ID value to RequirementsActivity
-                                            intent.putExtra("kitRequirements", requirementsList);
-
-                                            backToGuide.setVisibility(View.VISIBLE);
-                                            backToGuide.setOnClickListener(new View.OnClickListener() {
+                                            goToSpaceDetector.setVisibility(View.VISIBLE);
+                                            goToSpaceDetector.setOnClickListener(new View.OnClickListener() {
                                                 @Override
                                                 public void onClick(View v) {
-                                                    MainActivity.this.startActivity(intent);
+                                                    MainActivity.this.startActivity(spaceDetectorIntent);
                                                 }
                                             });
                                         } else {
@@ -396,25 +234,16 @@ public class MainActivity extends AppCompatActivity {
                                         scannedValues[1] = valueScanned;
 
                                         if((Arrays.toString(scannedValues)).equals("[x, x]")) {
-                                            final Toast toast = Toast.makeText(getApplicationContext(), "List item completed", Toast.LENGTH_SHORT);
-                                            toast.show();
-                                            Handler handler = new Handler();
-                                            handler.postDelayed(new Runnable() {
-                                                @Override
-                                                public void run() {
-                                                    toast.cancel();
-                                                }
-                                            }, 800);
 
-                                            requirementsList[listPosition] = "completed";
-                                            intent.putExtra("kitId", kitOneId);                      //Sending the kit ID value to RequirementsActivity
-                                            intent.putExtra("kitRequirements", requirementsList);
+                                            spaceDetectorIntent.putExtra("listPosition", listPosition);
+                                            spaceDetectorIntent.putExtra("kitId", kitOneId);                      //Sending the kit ID value to RequirementsActivity
+                                            spaceDetectorIntent.putExtra("requirementsList", requirementsList);
 
-                                            backToGuide.setVisibility(View.VISIBLE);
-                                            backToGuide.setOnClickListener(new View.OnClickListener() {
+                                            goToSpaceDetector.setVisibility(View.VISIBLE);
+                                            goToSpaceDetector.setOnClickListener(new View.OnClickListener() {
                                                 @Override
                                                 public void onClick(View v) {
-                                                    MainActivity.this.startActivity(intent);
+                                                    MainActivity.this.startActivity(spaceDetectorIntent);
                                                 }
                                             });
                                         } else {
--- app/src/main/java/com/example/afreecar/OpenCV.java
+++ app/src/main/java/com/example/afreecar/OpenCV.java
@@ -2,121 +2,187 @@ package com.example.afreecar;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.hardware.Camera;
 import android.os.Bundle;
-import android.os.Environment;
+import android.os.Handler;
 import android.provider.MediaStore;
-import android.util.Log;
-import android.view.MotionEvent;
-import android.view.SurfaceView;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import org.opencv.android.BaseLoaderCallback;
-import org.opencv.android.CameraBridgeViewBase;
-import org.opencv.android.LoaderCallbackInterface;
-import org.opencv.android.OpenCVLoader;
-import org.opencv.core.CvType;
+import org.opencv.android.Utils;
 import org.opencv.core.Mat;
-import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.MatOfPoint2f;
+import org.opencv.core.Point;
+import org.opencv.core.RotatedRect;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 
-import java.io.File;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.ArrayList;
+import java.util.List;
 
-public class OpenCV extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2, View.OnTouchListener {
+public class OpenCV extends AppCompatActivity {
 
-    private CameraBridgeViewBase mOpenCvCameraView;
-    private Mat mRgba;
-    private Mat mByte;
-    private Mat imGgray, imgCandy;
-
+    ImageView im; //Your image View
+    double distanceBetween;
+    Button launchOpenCv;
+    Button backToGuide;
+    public final String kitOneId = "assembly-requirements-one"; // This also needs to be stored in the database. Still need to implement compatibility for multiple different types of Kits. Right now, only one kit is being used.
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         setContentView(R.layout.opencv);
-        mOpenCvCameraView = findViewById(R.id.test);
-        findViewById(R.id.test).setOnTouchListener(this);
-        mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
-        mOpenCvCameraView.setCvCameraViewListener(this);
+        im = findViewById(R.id.imageView);
+        launchOpenCv = findViewById(R.id.launchOpenCv);
+        backToGuide = findViewById(R.id.backToGuide);
+
+        launchOpenCv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent img = new Intent (); //Your Intent
+                img.setAction(MediaStore.ACTION_IMAGE_CAPTURE); //the intents action to capture the image
+                startActivityForResult(img,1);//start the activity adding any code .1 in this example
+            }
+        });
     }
 
-    @Override
-    public void onPause()
-    {
-        super.onPause();
-        if (mOpenCvCameraView != null)
-            mOpenCvCameraView.disableView();
+    public double euclideanDistance(Point a, Point b){
+        double distance = 0.0;
+        try{
+            if(a != null && b != null){
+                double xDiff = a.x - b.x;
+                double yDiff = a.y - b.y;
+                distance = Math.sqrt(Math.pow(xDiff,2) + Math.pow(yDiff, 2));
+            }
+        }catch(Exception e){
+            System.err.println("Something went wrong in euclideanDistance function in  "+e.getMessage());
+        }
+        return distance;
     }
 
-    public void onDestroy() {
-        super.onDestroy();
-        if (mOpenCvCameraView != null)
-            mOpenCvCameraView.disableView();
+    /**
+     * Sorts contours from left to right (assumes a length of 2)
+     *
+     * @param contours
+     *            List of contours to sort
+     */
+    public ArrayList sortContours(ArrayList<MatOfPoint> contours) {
+        if (getLeftMostX(contours.get(0)) > getLeftMostX(contours.get(1))) {
+            contours.add(contours.remove(0));
+        }
+
+        return contours;
     }
 
-    public void onCameraViewStarted(int width, int height) {
-        mRgba = new Mat(height, width, CvType.CV_8UC4);
-        mByte = new Mat(height, width, CvType.CV_8UC4);
-        imGgray = new Mat(height, width, CvType.CV_8UC4);
-        imgCandy = new Mat(height, width, CvType.CV_8UC1);
-    }
-
-    public void onCameraViewStopped() {
-        mRgba.release();
-    }
-
-    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
-        mRgba = inputFrame.rgba();
-        Imgproc.cvtColor(mRgba, imGgray, Imgproc.COLOR_RGB2GRAY);
-        Imgproc.adaptiveThreshold(imGgray, mByte, 255, Imgproc.ADAPTIVE_THRESH_MEAN_C, Imgproc.THRESH_BINARY, 35, 5);
-        return mByte; // this is m Binary image
-    }
-
-    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
-        @Override
-        public void onManagerConnected(int status) {
-            switch (status) {
-                case LoaderCallbackInterface.SUCCESS:
-                {
-                    mOpenCvCameraView.enableView();
-                } break;
-                default:
-                {
-                    super.onManagerConnected(status);
-                } break;
+    /**
+     * @param points
+     *            List of points representing a contour
+     * @return The X coordinate of the left-most point in {@code points}
+     */
+    public double getLeftMostX(MatOfPoint points) {
+        List<Point> coords = points.toList();
+        double leftMostX = coords.get(0).x;
+        for (int i = 0; i < coords.size(); i++) {
+            if (coords.get(i).x < leftMostX) {
+                leftMostX = coords.get(i).x;
             }
         }
-    };
-
-    @Override
-    public void onResume()
-    {
-        super.onResume();
-        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION, this, mLoaderCallback);
+        return leftMostX;
     }
 
-    @Override
-    public boolean onTouch(View v, MotionEvent event) {
-        Intent img = new Intent (); //Your Intent
-        img.setAction(MediaStore.ACTION_IMAGE_CAPTURE); //the intents action to capture the image
-        startActivityForResult(img,1);//start the activity adding any code .1 in this example
-        return true;
-    }
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        List<RotatedRect> boxes = new ArrayList<>();
 
-    protected void onActivityResult(int requestCode,int resultCode,Intent data) {
         super.onActivityResult(requestCode, resultCode, data);//method retrieves the requestCode , its result and the data containing the pic from system
         if (requestCode == 1 && resultCode == RESULT_OK) {
             Bitmap photo = (Bitmap) data.getExtras().get("data"); //get data and casts it into Bitmap photo
-//            im.setImageBitmap(photo);// set photo to imageView
+
+            Mat img = new Mat();
+            Utils.bitmapToMat(photo, img);
+
+            Mat imageGray = new Mat();
+
+            //load the image, convert it to grayscale, and blur it slightly
+            Imgproc.cvtColor(img, imageGray, Imgproc.COLOR_RGB2GRAY);
+            Imgproc.GaussianBlur(imageGray, imageGray, new Size(7, 7), 0);
+
+            //perform edge detection, then perform a dilation + erosion to
+            //close gaps in between object edges
+            Mat edged = new Mat();
+            Imgproc.Canny(imageGray, edged, 50, 100);
+            Imgproc.dilate(edged, edged, Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(2,2)));
+            Imgproc.erode(edged, edged, Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(2,2)));
+
+            //find contours in the edge map
+            ArrayList<MatOfPoint> contours = new ArrayList<MatOfPoint>();
+            Mat hierarchy = new Mat();
+            Imgproc.findContours(edged, contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+
+            //sort the contours from left-to-right and, then initialize the
+            //distance colors and reference object
+            sortContours(contours);
+
+            //loop over the contours individually
+            for (MatOfPoint contour : contours) {
+                //compute the rotated bounding box of the contour
+                RotatedRect box = Imgproc.minAreaRect(new MatOfPoint2f(contour.toArray()));
+                boxes.add(box);
+            }
+
+            Scalar yellow = new Scalar(255, 255, 0);
+            Scalar blue = new Scalar(0, 0, 255);
+
+            Imgproc.circle(img, boxes.get(0).center, 2, yellow);
+
+            if(boxes.size() > 1) {
+                Imgproc.circle(img, boxes.get(1).center, 2, blue);
+                Imgproc.line(img, boxes.get(0).center, boxes.get(1).center, yellow, 2);
+                distanceBetween = Math.ceil(euclideanDistance(boxes.get(0).center, boxes.get(1).center));
+            }
+
+            Utils.matToBitmap(img, photo);
+
+            im.setImageBitmap(photo);// set photo to imageView
+        }
+
+        if(distanceBetween < 90 && distanceBetween > 80) {
+            final Intent assembleIntent = getIntent();
+            final String[] requirementsList = assembleIntent.getStringArrayExtra("requirementsList");
+            final int listPosition = assembleIntent.getIntExtra("listPosition", 0);
+
+            final Toast toast1 = Toast.makeText(getApplicationContext(), "List item completed", Toast.LENGTH_SHORT);
+            toast1.show();
+            Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    toast1.cancel();
+                }
+            }, 800);
+
+            final Intent intent = new Intent(OpenCV.this, RequirementsActivity.class);
+
+            requirementsList[listPosition] = "completed";
+            intent.putExtra("kitId", kitOneId);                      //Sending the kit ID value to RequirementsActivity
+            intent.putExtra("kitRequirements", requirementsList);
+
+            backToGuide.setVisibility(View.VISIBLE);
+            backToGuide.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    OpenCV.this.startActivity(intent);
+                }
+            });
+        } else {
+            final Toast toast1 = Toast.makeText(getApplicationContext(), "Insufficient connection, please try again. Distance: " + distanceBetween, Toast.LENGTH_SHORT);
+            toast1.show();
         }
     }
 }
